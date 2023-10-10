b51cd044cae8090114be1c3ffc152b20f4d8f3ec
ayoub_benayyad <ayoubbenayyad2017@gmail.com>
2023-10-10 11:31:22 +0100

    front end completed

--- app/src/main/java/ma/ensaf/myfirstapplication/MainActivity.java
+++ app/src/main/java/ma/ensaf/myfirstapplication/MainActivity.java
@@ -13,19 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
-    public void changeTxt(View view){
 
-        EditText firstNameInput= findViewById(R.id.FirstNameInput);
-        EditText LastNameInput= findViewById(R.id.LastNameInput);
-
-        TextView firstName= findViewById(R.id.FirstName);
-        TextView lastName= findViewById(R.id.LastName);
-
-        firstName.setText(firstNameInput.getText().toString());
-        lastName.setText(LastNameInput.getText().toString());
-
-    }
 }
